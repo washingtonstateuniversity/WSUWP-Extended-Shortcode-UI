2866163f9a3b06978dbba52190d3750db24d62a7
Jeremy Felt <jeremy.felt@gmail.com>
2017-05-23 13:11:42 -0700

    Reorganize plugin to match plugin skeleton structure
    Also updates Travis and tests config

--- plugin.php
+++ plugin.php
@@ -8,19 +8,16 @@ Author URI: https://web.wsu.edu/
 Plugin URI: https://github.com/washingtonstateuniversity/WSUWP-Extended-Shortcode-UI
 */
 
-namespace WSU\Shortcode_UI;
-
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-add_action( 'plugins_loaded', 'WSU\Shortcode_UI\bootstrap' );
-/**
- * Loads the rest of the plugin.
- *
- * @since 0.0.1
- */
-function bootstrap() {
-	include_once( __DIR__ . '/includes/image-shortcake.php' );
+// This plugin uses namespaces and requires PHP 5.3 or greater.
+if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
+	add_action( 'admin_notices', create_function( '',
+	"echo '<div class=\"error\"><p>" . __( 'WSUWP Extended Shortcode UI requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'wsuwp-extended-shortcode-ui' ) . "</p></div>';" ) );
+	return;
+} else {
+	include_once __DIR__ . '/includes/shortcode-ui.php';
 }
---
+++ tests/test-wsuwp-extended-shortcode-ui.php
@@ -0,0 +1,10 @@
+<?php
+
+class WSUWP_Extended_Shortcode_UI_Tests extends WP_UnitTestCase {
+	/**
+	 * An initial sample test to verify working tests.
+	 */
+	public function test_assert_true_works() {
+		$this->assertTrue( true );
+	}
+}
